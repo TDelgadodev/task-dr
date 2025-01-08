8f0c18034c1a118c61472fecab13f8ea94ab248b
Thiago <thiagodelgado.dev@gmail.com>
2025-01-08 20:52:37 -0300

    configs global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(morgan('dev'));
+  app.setGlobalPrefix('api');
+  app.enableCors(CORS);
+  const configService = app.get(ConfigService);  
+  await app.listen(configService.get('PORT'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
